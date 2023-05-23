3408712acb747a0652edfd07d4decdcffbd19144
sbockel <131286024+sbockel@users.noreply.github.com>
2023-05-23 15:09:08 +0200

    on vire ants

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(install_requires=['torch',
                         'numba',
                         'argparse',
                         'numpy',
-                        'antspyx',
                         # 'antspyx @ git+https://github.com/ANTsX/ANTsPy.git@v0.2.6#egg=antspyx',
                         'HD-BET @ git+https://github.com/MIC-DKFZ/HD-BET.git#egg=HD-BET',
                         ], **args)
